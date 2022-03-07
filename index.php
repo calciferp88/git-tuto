530f512ca21692b811e22408f528a359721c86f0
Red-rosetail <rosetail881@gmail.com>
2022-03-07 21:54:50 +0630

    i luv hmw

---
+++ index.php
@@ -0,0 +1,489 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>JT Commerce</title>
+	<meta charset="UTF-8">
+ 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+
+ 	<!-- Place favicon.ico in the root directory -->
+  	<link rel="shortcut icon" href="assets/img/fav1.PNG">
+
+	<!-- local style links -->
+	<!-- <link rel="stylesheet" type="text/css" href="assets/css/custom.css"> -->
+
+	<!-- Tailwind cdn -->
+	<script src="https://cdn.tailwindcss.com"></script>
+
+	<!-- Font awesome -->
+  	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
+</head>
+
+<body>
+	<!-- Header section start -->
+	<header class="xl:mx-[100px]">
+		<div class="flex justify-between">
+			<img 
+				class="h-[100px] xl:w-[250px] mt-5" 
+				src="assets/img/logo.PNG" 
+				alt="JTC Logo"
+			>
+
+			<div class='pt-5 md:bg-[#db9a7b] sm:bg:whitemr-4 h-[90px]'>
+				<a 
+					href="#contact" 
+					class="p-5 text-[#db9a7b] md:text-white mt-[50px] md:mt-[0px] robo-bold flex items-center justify-between xl:text-lg text-[16px] contact-now">
+						Contact us Now
+					<i class="fas fa-chevron-circle-down ml-3 hidden md:block xl:block"></i>
+				</a>
+			</div>
+		</div>
+	</header>
+	<!-- Header end -->
+
+	<!-- Banner section start -->
+	<div class="first-banner-bg mt-5">
+		<div class="p-5 text-white">
+			<center class="mt-[130px] text-3xl md:text-4xl xl:text-5xl robo-bold intro">
+				<h1>	
+					Manufacture and Distribution <br/>
+					of Stainless Steel Products
+				</h1>
+				
+			</center>
+			<div class="banner-icons mt-[50px]">
+				<div></div>
+				<div class="mt-[50px]">
+					<img src="assets/img/bannerIcon1.png" class="xl:h-[65px] md:h-[60px] h-[40px] -mt-[10px]">
+					<div class="">
+						<i class="far fa-star text-[7px] md:text-[10px]"></i>
+						<i class="far fa-star text-[7px] md:text-[10px]"></i>
+						<i class="far fa-star text-[7px] md:text-[10px]"></i>
+						<i class="far fa-star text-[7px] md:text-[10px]"></i>
+						<i class="far fa-star text-[7px] md:text-[10px]"></i>
+					</div>
+				</div>
+				<div class="relative mt-[50px]">
+					<p class="text-center text-[10px] md:text-[18px] absolute -top-5 right-7">24h</p>
+					<img src="assets/img/bannerIcon2.png" class="xl:h-[65px] md:h-[60px] h-[40px] mt-[1px]">
+				</div>
+				<div class="mt-[50px]"><img src="assets/img/bannerIcon3.png" class="xl:h-[65px] md:h-[60px] h-[40px] block"></div>
+				<div class="mt-[50px]"><img src="assets/img/bannerIcon4.png" class="xl:h-[65px] md:h-[60px] h-[40px] block"></div>
+				<div></div>
+			</div>
+		</div>
+	</div>
+	<!-- Banner section end -->
+
+	<!-- Residential Products -->
+	<div class="xl:mx-[100px] mx-[15px] mt-[40px] xl:mt-[100px] pb-7">
+		<div class="border-b border-gray-500 pb-7">
+			<h2 class="text-3xl text-gray-700">Residential Products</h2>
+		</div>
+
+		<!-- resdential grid start -->
+		<div class="grid sm:grid-cols-1 sm:gap-1 md:grid-cols-2 xl:grid-cols-3 xl:gap-3 mt-3">
+		  <div class="mb-[5px] bg-[#f6f6f6]">
+		  	<img src="assets/img/residential1.png" class="object-contain">
+		  	<p class="p-2 pb-5 border-b border-b-2 border-[#db9a7b] text-lg text-gray-700 hover:border-b-[3px] delay-100">
+		  		Cut to measure backsplash panels
+		  	</p>
+		  </div>
+
+		  <div class="mb-[5px] bg-[#f6f6f6]">
+		  	<img src="assets/img/resres.png" class="object-contain"> 
+		  	<p class="p-2 pb-5 border-b border-b-2 border-[#db9a7b] text-lg text-gray-700 hover:border-b-[3px] delay-100">
+		  		Various shelving design options
+		  	</p>
+		  </div>
+
+		  <div class="mb-[5px] bg-[#f6f6f6]">
+		  	<img src="assets/img/res3.png" class="object-contain">
+		  	<p class="p-2 pb-5 border-b border-b-2 border-[#db9a7b] text-lg text-gray-700 hover:border-b-[3px] delay-100">
+		  		Cookware bars for all your pots and pans
+		  	</p>
+		  </div>
+
+		  <div class="">
+		  	<p class="text-xl text-gray-500 p-3">
+		  		We manufacture a variety of backsplash products including panels, backsplash with integrated shelves and/or cookware bars. We also offer stainless steel tiles and other household products such as grilling utensil sets that hold up to a lifetime of abuse.
+		  	</p>
+		  </div>
+		</div>
+		<!-- resdential grid end -->
+		<p class="text-gray-500 p-3 text-xl font-bold">
+			Backsplash With Shelf or Bar  |  Backsplash Tiles  |  <br>
+			Grilling Accessories  |  Kitchen Accessories  |  Household Products
+		</p>
+	</div>
+	<!-- Residential Products end -->
+
+	<!-- Restaurants section  -->
+	<div class="bg-[#f6f6f6] w-100 p-3">
+		<div class="grid sm:grid-cols-1 sm:gap-1 xl:grid-cols-2 xl:gaps-2">
+			<div class="xl:mx-[100px] mx-[15px] mt-[40px] md:mt[100px] pb-7 md:mx-[100px]">
+				<h2 class="border-b border-b-2 border-gray-500 pb-3 text-3xl">
+					Resaurants
+				</h2>
+				<p class="text-gray-500 leadin-7 text-xl pt-5">
+					Many suppliers think of restaurants as a never-ending cash cow, but we’d like to think differently. For example our stainless steel beer flights have no equal, and will last 10x longer than any wooden flight, and are far more sanitary.
+				</p>
+				<div class="flex items-center mt-7 md:mx-[100px]">
+					<div class="border-r border-r-2 border-[#db9a7b] p-3 pr-7">
+						<img src="assets/img/restaurant1.png" width="180">
+					</div>
+					<div class="ml-3">
+						<p class="text-2xl text-gray-700">	
+							Extensive <br>
+							Range of <br>
+							Finishes <br>
+						</p>
+					</div>
+				</div>
+			</div>
+			<div class="mt-[100px] restaurant-bg relative md:mx-[100px] xl:mx-0 rest-img">
+				<img 
+					src="assets/img/restaurant3.png" 
+					width="200" 
+					class="absolute bottom-20 right-[23%] xl:-top-20 xl:right-5 md:-top-20 md:-right-10 rest-img"
+				>
+			</div>
+
+		</div>
+
+		<p class="text-gray-500 leadin-7 text-center -mt-[80px] text-xl pt-5 xl:mx-[100px] tracking-wider xl:mt-10 xl:text-left md:text-center md:mt-[-400px] mb-5 beer-flight">
+			Beer Flights  |  Wall Trims  |  Kick Plates  |  Corner Guards  |  Tables  |  Shelving
+		</p>
+	</div>
+	<!-- Restaurant section end -->
+	
+	<!-- trim section start -->
+	<div class="xl:mx-[100px] mx-[30px] mt-[40px] md:mt-[100px] pb-7 md:mx-[100px]">
+		<div class="border-b border-gray-500 pb-7">
+			<h2 class="text-3xl text-gray-700">Trim</h2>
+		</div>
+
+		<p class="text-xl text-left text-gray-500  xl:w-[500px] my-5 ">
+			Stainless steel sheets are very popular in applications such as restaurants, hospitals, food packaging facilities, commercial kitchens, etc, etc, and our trim is used for joining, edging, and for inside and outside corners.
+		</p>
+		<!-- resdential grid start -->
+		<div class="grid sm:grid-cols-1 sm:gap-1 md:grid-cols-2 xl:grid-cols-4 xl:gap-4 mt-3">
+		  <div class="mb-[5px] lol">
+		  	<img src="assets/img/outside-corner.PNG" class="h-[400px] md:h-[440px] lg:ml-[0px] ml-[22px] ">
+		  </div>
+
+		  <div class="mb-[5px] lol">
+		  	<img src="assets/img/outside-corner.PNG" class="h-[400px] md:h-[440px] lg:ml-[0px] ml-[22px] md:ml-[30px]  "> 
+		  </div>
+
+		  <div class="mb-[5px] lol">
+		  	<img src="assets/img/outside-corner.PNG" class="h-[400px] md:h-[440px] lg:ml-[0px] ml-[22px] "> 
+		  </div>
+
+		  <div class="mb-[5px] lol">
+		  	<img src="assets/img/outside-corner.PNG" class="h-[400px] md:h-[440px] lg:ml-[0px] ml-[22px] md:ml-[30px] "> 
+		  </div>
+
+		</div>
+
+		<p class="text-gray-500 leading-7 text-center text-xl pt-5 tracking-wider xl:mt-10 xl:text-left md:text-center">
+			Divider Trim  |  Edge Trim  |  Inside and Outside Corner Trims  |  Seam Trim  |  Cap Trim  |  Custom Trims
+		</p>
+	</div>
+	<!-- trim section end -->
+
+	<!-- colored stainless steel section start-->
+	<div class="bg-[#f6f6f6] w-100 p-3 pb-[60px]">
+		<div class="grid sm:grid-cols-1 sm:gap-1 xl:grid-cols-2 xl:gaps-2">
+			<h2 class="block md:hidden xl:hidden mx-[10px] border-b border-b-2 border-gray-500 pb-3 text-3xl mt-[40px] xl:mt-0">
+				Colored Stainless Steel
+			</h2>
+
+			<!-- desktop and phone size togogle text -->
+			<div class="xl:mx-[100px] mx-[15px] mt-[100px] pb-7 md:mx-[100px] hidden md:block xl:block">
+				<h2 class="border-b border-b-2 border-gray-500 pb-3 text-3xl">
+					Colored Stainless Steel
+				</h2>
+				<p class="text-gray-500 leadin-7 text-lg pt-5">
+					Stainless steel is expensive and popular, but the price can cause a bit of an upset stomach at times. Traditional stainless steel coloring options such as dipping and PVD add tremendously to this cost, so we have been looking for alternatives. Our laminated options are far less expensive and take care of multiple problems. Benefits include the following:
+				</p>
+				<ul class="text-gray-500 leadin-7 text-md pt-5 mt-3 hidden xl:block">
+					<li class="my-1">. The corrosion resistance of 304 stainless steel.</li>
+					<li class="my-1">. The magnetic properties of 430 stainless steel.</li>
+					<li class="my-1">. Color uniformity that outperforms LIC and traditional PVD.</li>
+					<li class="my-1">.  Much longer lifespan versus PVD options.</li>
+				</ul>
+			</div>
+
+			<div class="mt-[4rem] xl:mt-[100px] stainless-bg relative mx-[10px] md:mx-[100px] relative">
+
+				<!-- black bg -->
+				<div class="black-bg">
+					<img src="assets/img/stainless-rigth2.png" class="h-full">
+				</div>
+
+				<!-- building -->
+				<img src="assets/img/stainlessleft.png" class="hidden xl:block md:block right-0 md:-left-[12%] xl:h-[45vh] md:h-[35vh] h-[40vh] absolute top-[17%] xl:top-[23%] right-[50%]"> 
+
+				<!-- colors bg -->
+				<div class="colors-bg">
+					<img 
+						src="assets/img/color-gp.png" 
+						class="h-[79vh] xl:h-[75vh] md:h-[70vh] xl:top-[-2%] md:top-[-8%] absolute top-[-9%] -right-5 md:right-[-13%] xl:right-[-9%]">
+				</div>
+			</div>
+
+			<!-- toogle text for phone size -->
+			<div class="xl:mx-[100px] mx-[15px] mt-[3rem] xl:mt-[100px] pb-7 md:mx-[100px] block md:hidden xl:hidden">
+				<h2 class="hidden xl:block border-b border-b-2 border-gray-500 pb-3 text-3xl">
+					Colored Stainless Steel
+				</h2>
+				<p class="text-gray-500 leadin-7 text-lg pt-5">
+					Stainless steel is expensive and popular, but the price can cause a bit of an upset stomach at times. Traditional stainless steel coloring options such as dipping and PVD add tremendously to this cost, so we have been looking for alternatives. Our laminated options are far less expensive and take care of multiple problems. Benefits include the following:
+				</p>
+			</div>
+
+		</div>
+	</div>
+
+	<!-- colored stainless steel section end -->
+
+	<!-- Speciality  Products -->
+	<div class="xl:mx-[100px] mx-[15px] mt-[100px] pb-7">
+		<div class="border-b border-gray-500 pb-7">
+			<h2 class="text-3xl text-gray-700 font-black font-bold">Speciality Products</h2>
+		</div>
+
+		<!-- resdential grid start -->
+		<div class="grid sm:grid-cols-1 sm:gap-1 md:grid-cols-2 xl:grid-cols-3 xl:gap-3 mt-3">
+		  <div class="mb-[5px]">
+		  	<p class="text-lg text-gray-700 xl:text-left text-center md:text-center">
+		  		Good-looking and precise solutions to your pesky problems. Custom tailored designs for trims, shelving, surface protection, etc, made quickly, to tight tolerances, and with precision; that means excellent part consistency.
+		  	</p>
+		  </div>
+
+		  <div class="mb-[5px] xl:hidden width-[0px]">
+		  </div>
+
+		  <div class="mb-[5px]">
+		  	<img src="assets/img/special1.png" class="h-[270px]"> 
+		  </div>
+
+		  <div class="mb-[5px]">
+		  	<img src="assets/img/special2.png" class="h-[270px]">
+		  </div>
+		</div>
+		<!-- resdential grid end -->
+		<p class="text-gray-700 p-3 text-xl">
+			Backsplash With Shelf or Bar  |  Backsplash Tiles  |  <br>
+			Grilling Accessories  |  Kitchen Accessories  |  Household Products
+		</p>
+	</div>
+	<!-- Speciality Products end -->
+
+	<!-- Form section start-->
+	<div class="bg-[#f6f6f6] w-100 p-3 pb-[60px] border-t-2 border-[#db9a7b]">
+
+		<form class="w-full justify-center mx-auto mt-[70px]">
+
+			<div class="mx-[1vh] md:mx-[5vh] xl:mx-[32vh]">
+				<!-- 2 cols input (1) -->
+			  	<div class="flex flex-wrap -mx-3 mb-6">
+
+				    <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+				      <label 
+				      	class="block block text-gray-700 text-lg font-bold mb-2" 
+				      	for="grid-first-name"
+				      >
+				        Name*
+				      </label>
+				      <input class="block w-full bg-white text-gray-700 py-3 px-4 mb-3 border-b-2 border-[#db9a7b]" type="text">
+				    </div>
+
+				    <div class="w-full md:w-1/2 px-3">
+				      <label class="block block text-gray-700 text-lg font-bold mb-2" for="grid-first-name">
+				        Email*
+				      </label>
+				       <input class="block w-full bg-white text-gray-700 py-3 px-4 mb-3 border-b-2 border-[#db9a7b]" type="email">
+				    </div>
+			  	</div>
+
+			  	<!-- 2 cols input (2) -->
+			  	<!-- 2 cols input -->
+			  	<div class="flex flex-wrap -mx-3 mb-6">
+
+				    <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+				      <label 
+				      	class="block block text-gray-700 text-lg font-bold mb-2" 
+				      	for="grid-first-name"
+				      >
+				        Phone
+				      </label>
+				      <input class="block w-full bg-white text-gray-700 py-3 px-4 mb-3 border-b-2 border-[#db9a7b]" type="text">
+				    </div>
+
+				    <div class="w-full md:w-1/2 px-3">
+				      <label class="block block text-gray-700 text-lg font-bold mb-2" for="grid-first-name">
+				        Company
+				      </label>
+				       <input class="block w-full bg-white text-gray-700 py-3 px-4 mb-3 border-b-2 border-[#db9a7b]" type="email">
+				    </div>
+			  	</div>
+
+			  	<!-- 1 col input -->
+			  	<div class="flex flex-wrap -mx-3 mb-6">
+				    <div class="w-full px-3">
+				      <label class="block text-gray-700 text-lg font-bold mb-2" >
+				        Message*
+				      </label>
+				      <input class="block w-full bg-white text-gray-700 py-3 px-4 mb-3 border-b-2 border-[#db9a7b] h-[200px]" id="grid-password" type="password">
+				    </div>
+			  	</div>
+
+			  	<button 
+			  		type="button" 
+			  		class="text-gray-900 bg-gradient-to-r from-red-200 via-red-300 to-yellow-200 hover:bg-gradient-to-bl focus:ring-4 focus:ring-red-100 w-full dark:focus:ring-red-400 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+			  	>
+			  		Red to Yellow
+			  	</button>
+			</div>
+
+		</form>
+	</div>
+
+	<!-- form section end -->
+</body>
+
+	<!-- scripts files  -->
+ 	<script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
+
+
+ 	<style type="text/css">
+ 		@import url('https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@300&display=swap');
+
+		/*---------- global styles -------*/
+		*{
+			font-family: 'Roboto Condensed', sans-serif;
+			font-weight: bold;
+		}	
+
+		a{
+			border: none;
+			outline:none;
+		}
+
+		.bg-copper{
+			background-color: rgba(219, 154, 123, 1);
+		}
+
+		.txt-copper{
+			color: rgba(219, 154, 123, 1);
+		}
+
+		/*-------- custom styles -----*/
+		.first-banner-bg{
+			background-image: url("assets/img/bannerbg2.jpeg");
+			background-size: cover;
+			height: auto;
+			background-position: center;
+			background-repeat: no-repeat;
+			padding-bottom: 7rem;
+		}
+
+		.banner-icons{
+			display: flex;
+			margin: 0 auto;
+			justify-content: space-around;
+			width: auto;
+		}
+
+		.restaurant-bg{
+			background-image: url("assets/img/restaurant2.png");
+			background-size: fill;
+			height: 80vh;
+			background-repeat: no-repeat;
+		}
+
+		/* iPhone SE (37px)*/
+		@media only screen and (min-width: 375px){
+			.rest-img img{
+			  	top: 74%;
+			  	right:20%;
+			  }
+			  .beer-flight{
+			  	margin-top: 0%;
+			  }
+		}
+
+		/*  Android (37px)*/
+		@media only screen and (max-width: 414px){
+			.rest-img img{
+			  	top: 65%;
+			  	right:20%;
+			  }
+			  .beer-flight{
+			  	margin-top: 5%;
+			  }
+
+			   .colors-bg img{
+			  	height: 60vh;
+			  	top:-6%;
+			  }
+		}
+
+		/* Surface Duo / Mini Tablet */
+		@media only screen and (min-width: 540px) {
+		  .lol img {
+		    text-align: center;
+		    margin-left: 7.6rem;
+		  }
+
+		  .rest-img img{
+		  	top: -20%;
+		  	right:-1%;
+		  }
+
+		  .intro h1{
+		  	margin-bottom: 2rem;
+		  }
+
+		  .black-bg img{
+		  	height: 70vh;
+		  	width: 100%;
+		  }
+
+		  .colors-bg img{
+		  	top: -4%;
+		  }
+
+		}
+
+		/* iPad Air */
+		@media only screen and (min-width: 820px) {
+		  .lol img {
+		    margin-left: 0rem;
+		  }	
+		  .rest-img img{
+		  	top: -10%;
+		  	right:-1%;
+		  }
+		}
+
+
+
+		/*i phone SE*/
+		@media only screen and (min-width: 375px) {
+		  .intro h1{
+		  	margin-bottom: 2rem;
+		  }
+		  .rest-img{
+		  	margin-bottom: 3rem;
+		  }
+		  .rest-img img{
+		  	bottom: 0;
+		  }
+
+		}
+ 	</style>
+
+</html>
